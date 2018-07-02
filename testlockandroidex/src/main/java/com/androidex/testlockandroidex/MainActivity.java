8f73cd9367d358e420367be6523728de74ebd38a
liyp <liyp@androidex.cn>
2018-07-02 09:23:33 +0800

    使用旧的地址开门

--- testlockandroidex/src/main/java/com/androidex/testlockandroidex/MainActivity.java
+++ testlockandroidex/src/main/java/com/androidex/testlockandroidex/MainActivity.java
@@ -44,8 +44,8 @@ public class MainActivity extends AppCompatActivity implements OnBackCall {
     }
 
     public class DoorLockServiceBinder extends IDoorLockInterface.Stub {
-        //String rkeyDev = "/dev/rkey";
-        String rkeyDev = "/dev/ttySV0";
+        String rkeyDev = "/dev/rkey";
+        //String rkeyDev = "/dev/ttySV0";
         int ident = 0;
 
         /**
@@ -60,12 +60,19 @@ public class MainActivity extends AppCompatActivity implements OnBackCall {
             if (index < 0 || index > 0xFE) index = 0;
             if (ident < 0 || ident > 0xFE) ident = 0;
             if (delay < 0 || delay > 0xFE) delay = 0;
+            // int result = mDoorLock.openDoor(0xF0, 0); //常开
+            // int result = mDoorLock.openDoor(0xF0, 0x40); //延时自动关门40*150ms
             String cmd = String.format("FB%02X5003%02X01%02X00FE", ident, index, delay);
             //echo "FB005003F0010000FE" > dev/ttySV0
+            //echo "FB005003F0010000FE" > dev/rkey
             //FB+Index+CMD+LEN+DATA+CRC+FE
-            //    FB 00 50 03 F0 01 00 00FE
-            //0-->FB 00 25 03 F0 01 00 00FE
-            //0x40 -->
+            //开门  FB 00 50 03 F0 01 00 00FE
+            //关门  FB 00 25 03 F0 01 00 00FE
+            //常开：        FB005003 F0 01 00 00FE
+            //延时自动关门：FB005003 F0 01 40 00FE
+            //全开：  FB005003 F0 01 40 00FE
+            //开主门：FB005003 00 01 40 00FE
+            //开附门：FB005003 01 01 40 00FE
             int r = rkey.native_file_writeHex(rkeyDev, cmd);
             Log.i("xiao_", "cmd = " + cmd);
             if (r > 0) {
